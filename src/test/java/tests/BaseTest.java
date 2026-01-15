d263b4e61a5e0f634d60e672471fe410239bd910
Nourhan Taha <norhan.taha.shawky@gmail.com>
2026-01-15 14:47:57 +0200

    Infrastructure Implemented

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -6,7 +6,6 @@ import drivers.WebDriverProvider;
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
-import pages.NavigationBarPage;
 import utils.JsonUtils;
 
 
